b8341a226f1e3f88ab39a7b22cd35095b7328716
OmarRyaad <its.omaarmohamed@gmail.com>
2025-06-18 16:30:22 +0300

    Push

--- src/screens/Servers/sections/CallToActionSection.jsx
+++ src/screens/Servers/sections/CallToActionSection.jsx
@@ -1,5 +1,5 @@
 import { Badge } from "../../../components/UI/Badge";
-import { Button } from "/components/ui/button";
+import { Button } from "../../../components/ui/button";
 import {
   Card,
   CardContent,
